82c6bd6be58061f9d2bf5bdb18f76a95b3bc5107
TasamayaNatalia <kesa-08_@mail.ru>
2024-01-13 22:56:48 +0300

    add content App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,12 +1,14 @@
 package hexlet.code;
 
 import java.util.Scanner;
+
+import static hexlet.code.Calc.calculate;
 import static hexlet.code.Cli.greeting;
 import static hexlet.code.Even.evenstarting;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n0 - Exit");
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc\n0 - Exit");
         Scanner scan = new Scanner(System.in);
         int number = scan.nextInt();
         System.out.println ("Your choice: " + number);
@@ -14,6 +16,8 @@ public class App {
             greeting();
         } else if (number == 2) {
             evenstarting();
+        } else if (number == 3) {
+            calculate();
         }
 
     }
