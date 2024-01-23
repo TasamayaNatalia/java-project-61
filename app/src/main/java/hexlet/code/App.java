834c55cdbdad2ff863c1cc2d3986f7ec4b052a7c
TasamayaNatalia <kesa-08_@mail.ru>
2024-01-23 17:19:21 +0300

    add new content App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,11 +6,13 @@ import static hexlet.code.Calc.calculate;
 import static hexlet.code.Cli.greeting;
 import static hexlet.code.Even.evenstarting;
 import static hexlet.code.GCD.findOfDivisor;
+import static hexlet.code.Prime.selectPrimeNumber;
+
 import static hexlet.code.Progression.completing;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n0 - Exit");
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
         Scanner scan = new Scanner(System.in);
         int number = scan.nextInt();
         System.out.println ("Your choice: " + number);
@@ -24,6 +26,8 @@ public class App {
             findOfDivisor();
         } else if (number == 5) {
         completing();
+        } else if (number == 6) {
+            selectPrimeNumber();
         }
 
     }
