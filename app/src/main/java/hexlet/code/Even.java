bbb4b5858396b99ab03447cd8ac918abc182e05b
TasamayaNatalia <kesa-08_@mail.ru>
2024-01-26 19:18:57 +0300

    add new content Even.java

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -18,22 +18,21 @@ public class Even {
             int number = (int) (Math.random() * 100);
             System.out.println("Question: " + number);
 
+            String correctAnswer = " ";
+            if(number % 2 == 0) {
+                correctAnswer = "yes";
+            } else {
+                correctAnswer = "no";
+            }
+
             String userAnswer = scanner.next();
             System.out.println("Your answer: " + userAnswer);
-            if (number % 2 == 0 && userAnswer.equals("yes")) {
+
+            if (userAnswer.equals(correctAnswer)) {
                 System.out.println("Correct!");
                 count++;
-            } else if (number % 2 != 0 && userAnswer.equals("no")) {
-                System.out.println("Correct!");
-                count++;
-            } else if (number % 2 != 0 && userAnswer.equals("yes")) {
-                System.out.println("'yes' is the wrong answer ;(. The correct answer was 'no'.\nLet's try again, " + userName + "!");
-                break;
-            } else if (number % 2 == 0 && userAnswer.equals("no")) {
-                System.out.println("'no' is the wrong answer ;(. The correct answer was 'yes'.\nLet's try again, " + userName + "!");
-                break;
             } else {
-                System.out.println(userAnswer + " is the wrong answer ;(.\nLet's try again, " + userName + "!");
+                System.out.println(userAnswer + " is wrong answer ;(. Correct answer was " + correctAnswer + ".\nLet's try again, " + userName + "!");
                 break;
             }
             //count = count + 1;
