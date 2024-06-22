eddc909c7cb883e9b52a8082c586884f128dda3f
titinkinanv <kesa-08_@mail.ru>
2024-06-22 15:13:04 +0300

    add content Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,40 +1,37 @@
 package hexlet.code.games;
 import hexlet.code.Engine;
 
+
 public class Calc {
+
     public static void game() {
 
-        String rules = "What is the result of the expression......?";
+        String rules = "What is the result of the expression?";
+
         String[][] questionsChecks = new String[Engine.ROUNDS][2];
+
 
         for (var questionsCheck : questionsChecks) {
             final int firstNumber = (int) (Math.random() * 100);
             final int secondNumber = (int) (Math.random() * 100);
+            final int numberOfOperator = (int) (Math.random() * 3);
 
             String[] operators = {" + ", " - ", " * "};
-            questionsCheck[0] = (firstNumber + operators[1] + secondNumber);
-            questionsCheck[1] = Integer.toString(calc(firstNumber, secondNumber, operators[1]));
+            questionsCheck[0] = (firstNumber + " " + operators[numberOfOperator] + " " + secondNumber);
+            questionsCheck[1] = Integer.toString(calc(firstNumber, secondNumber, operators[numberOfOperator]));
         }
 
         Engine.engine(rules, questionsChecks);
     }
 
     public static int calc(int firstNumber, int secondNumber, String operator) {
-        int result;
-
-        switch (operator) {
-            case "+":
-                result = firstNumber + secondNumber;
-                break;
-            case "-":
-                result = firstNumber - secondNumber;
-                break;
-            case "*":
-                result = firstNumber * secondNumber;
-                break;
-            default:
-                throw new Error("Unknown operator!");
-        }
-        return result;
+        return switch (operator) {
+            case "+" -> firstNumber + secondNumber;
+            case "-" -> firstNumber - secondNumber;
+            case "*" -> firstNumber * secondNumber;
+            default -> throw new Error("Unknown operator!");
+        };
     }
+
 }
+
