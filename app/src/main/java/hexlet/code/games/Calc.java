7b1a955c7c10176ec53002d474dc11abe1d4d481
titinkinanv <kesa-08_@mail.ru>
2024-06-23 10:56:26 +0300

    add content Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -10,30 +10,27 @@ public class Calc {
 
         String[][] questionsChecks = new String[Engine.ROUNDS][2];
 
-
         for (var questionsCheck : questionsChecks) {
             final int firstNumber = (int) (Math.random() * 100);
             final int secondNumber = (int) (Math.random() * 100);
             final int numberOfOperator = (int) (Math.random() * 3);
 
-
             String[] operators = {" + ", " - ", " * "};
             String operator = operators[numberOfOperator];
 
             questionsCheck[0] = (firstNumber + " " + operator + " " + secondNumber);
-            questionsCheck[1] = Integer.toString(calc(firstNumber, secondNumber, operator));
-
+            questionsCheck[1] = calc(firstNumber, secondNumber, operator);
         }
 
         Engine.engine(rules, questionsChecks);
     }
 
-    public static int calc(int firstNumber, int secondNumber, String operator) {
+    public static String calc(int firstNumber, int secondNumber, String operator) {
         return switch (operator) {
-            case "+" -> firstNumber + secondNumber;
-            case "-" -> firstNumber - secondNumber;
-            case "*" -> firstNumber * secondNumber;
-            default -> 0;
+            case " * " -> Integer.toString(firstNumber * secondNumber);
+            case " - " -> Integer.toString(firstNumber - secondNumber);
+            case " + " -> Integer.toString(firstNumber + secondNumber);
+            default -> null;
         };
     }
 }
